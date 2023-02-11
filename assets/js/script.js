7fc7b5dfb8e075243b7191fc6f82509a9b3ef39d
Jaswal1p <Jaswal1p@yahoo.com>
2023-02-10 23:07:15 -0500

    added code to capture & display text in input field

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,14 @@ var buttonEl = document.querySelector("#btn1");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function () {
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value; 
+    // console.log(taskNameInput);
+    
+
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
-    taskItemEl.textContent = "This is a new task";
+    taskItemEl.textContent = taskNameInput;
     tasksToDoEl.appendChild(taskItemEl);
 }
 
