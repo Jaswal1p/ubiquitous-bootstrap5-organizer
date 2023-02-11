baaf9b27f2afa4b01ca4445d13e84bd4993df3b9
Jaswal1p <Jaswal1p@yahoo.com>
2023-02-10 23:49:16 -0500

    added code to successfully capture input text & select fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,26 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function () {
 
     var taskNameInput = document.querySelector("input[name='task-name']").value; 
-    // console.log(taskNameInput);
+    console.log(taskNameInput);
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
     
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
 
-    var taskItemEl = document.createElement("li");
-    taskItemEl.className = "task-item";
-    taskItemEl.textContent = taskNameInput;
-    tasksToDoEl.appendChild(taskItemEl);
+    // create div to hold name of task & type
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // it needs some dynamic HTML
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "<h5><span class='task-type'>" + taskTypeInput + "</span>"
+    
+    listItemEl.appendChild(taskInfoEl);
+
+    // taskItemEl.className = "task-item";
+    // taskItemEl.textContent = taskNameInput;
+    tasksToDoEl.appendChild(listItemEl);
 }
 
 
